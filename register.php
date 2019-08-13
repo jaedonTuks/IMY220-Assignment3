53f183d25d5bdf3cc48638e68579c5ec4f70772e
u18058737 <54079026+u18058737@users.noreply.github.com>
2019-08-13 20:32:45 +0200

    Add files via upload

--- register.php
+++ register.php
@@ -24,7 +24,7 @@
 <head>
 	<meta charset="utf-8" />
 	<title>IMY 220 - Assignment 3</title>
-	<meta name="author" content="Name Surname">
+	<meta name="author" content="Jaedon Heger">
 	<!-- Replace Name Surname with your name and surname -->
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
